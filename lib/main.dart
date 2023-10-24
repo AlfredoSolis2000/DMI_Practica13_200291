43897750674c3e02e7d53709fac8d71e2dfc5029
AlfredoSolis2000 <alfredo.solissanchez@utxicotepec.edu.mx>
2023-10-23 22:00:30 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// Creado por: Alfredo Solis Sanchez
+// Asignatura: Desarrollo Movil Integral
+//Grado: 10   Grupo: "A"
+// Docente: MTI. Marco Antonio Ramirez Hernandez
+
+import 'package:flutter/material.dart';
+import 'package:movieapp_200644/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: 
+      Home(),
+      theme: new ThemeData.dark(),
+  ));
+}
